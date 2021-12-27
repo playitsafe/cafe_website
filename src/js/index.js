ef189da464296da199d127a44e18796ec928f5a7
playitsafe <playitsafe@qq.com>
2021-12-28 03:26:49 +1100

    feat: added reactive menu tab

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,10 @@
 import ready from "./init.js";
 import navCheckScroll from "./navbar.js";
+import slider from "./slider.js";
 
 const onReady = function () {
   window.onscroll = navCheckScroll;
+  slider()
 }
 
 ready(onReady);
