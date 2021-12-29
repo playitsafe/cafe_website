f055d1aaea3d1d67f47c5b3996fa5705a86bf854
playitsafe <playitsafe@qq.com>
2021-12-30 02:31:22 +1100

    feat: menu section animation added

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import ready from "./init.js";
 import navCheckScroll from "./navbar.js";
 import slider from "./slider.js";
+import waypoint from "./waypoints.js";
 
 const onReady = function () {
   window.onscroll = navCheckScroll;
