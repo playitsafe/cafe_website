44ac780b74afba66e8b13410ee3f54cab0bd5184
playitsafe <playitsafe@qq.com>
2022-01-03 02:37:08 +1100

    feat: map section added

--- src/js/index.js
+++ src/js/index.js
@@ -3,11 +3,13 @@ import navCheckScroll from "./navbar.js";
 import slider from "./slider.js";
 import waypoint from "./waypoints.js";
 import gallery from "./fancybox.js";
+import initMap from "./gmap.js";
 
 const onReady = function () {
   window.onscroll = navCheckScroll;
   slider()
-
+  initMap()
+  // console.log(initMap)
 
 
 }
